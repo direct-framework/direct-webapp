11f1a3b41e7acf1497fbf4fc4326524fc77563c7
Saranjeet Kaur <s.bhogal@imperial.ac.uk>
2025-07-29 14:05:52 +0100

    add to main/urls.py

--- direct_webapp/urls.py
+++ direct_webapp/urls.py
@@ -18,10 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from main import views
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("about/", views.AboutPageView.as_view(), name="about"),
 ]
