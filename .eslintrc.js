b209e1fbc921f7d4cd85a683799c4d341f8d86c3
Sam Bland <sam.bland@york.ac.uk>
2025-10-23 10:50:32 +0100

    Implement tests and some tidying up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,63 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
+module.exports = [
+  {
+    // Add rules for JavaScript files
+    files: ['**/*.{js}'],
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:storybook/recommended',
+    ],
+    plugins: ['prettier'],
+    parserOptions: {
+      ecmaVersion: 13,
+      sourceType: 'module',
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      // indent: ['error', 2], // Disabled as it conflicts with Prettier
+      semi: ['error', 'never'],
+      'no-var': 'error',
+    },
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-  plugins: ['prettier'],
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
+  {
+    // Add rules for Test files
+    files: ['**/*.test.{js}'],
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:storybook/recommended',
+    ],
+    plugins: ['prettier'],
+    parserOptions: {
+      ecmaVersion: 13,
+      sourceType: 'module',
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      // indent: ['error', 2], // Disabled as it conflicts with Prettier
+      semi: ['error', 'never'],
+      'no-var': 'error',
+    },
+    // Include testing globals for vitest
+    globals: {
+      vi: true,
+      describe: true,
+      it: true,
+      expect: true,
+      beforeEach: true,
+      afterEach: true,
+      beforeAll: true,
+      afterAll: true,
+    },
   },
-  rules: {
-    'prettier/prettier': 'error',
-    indent: ['error', 2],
-    semi: ['error', 'never'],
-    'no-var': 'error',
-  },
-}
+]
