17cc2893f5df8df0c01704111f6820d961777278
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2026-03-24 12:01:15 +0000

    Remove populate_db, the old framework data and all references to both

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Loading framework into Database..."
-python -m scripts.populate_db -j data/skills-competencies-framework.json
-
 echo "Starting Gunicorn..."
 exec "$@"
