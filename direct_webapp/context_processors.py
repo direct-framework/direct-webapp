5f9fa3fc9e39e083dc803e3eb23b8dc2a0f9b0df
Dave Horsfall <dave@horsfall.dev>
2026-04-17 07:17:17 +0100

    fix: switch to shields.io badges for release info

--- direct_webapp/context_processors.py
+++ direct_webapp/context_processors.py
@@ -3,14 +3,7 @@
 from django.conf import settings
 from django.http import HttpRequest
 
-from direct_webapp.github_releases import get_footer_release_links
-
 
 def site_title(request: HttpRequest) -> dict[str, str]:
     """Return the site title from settings."""
     return {"site_title": getattr(settings, "SITE_TITLE")}
-
-
-def footer_release_links(request: HttpRequest) -> dict[str, object]:
-    """Return footer release metadata for the configured GitHub repositories."""
-    return {"footer_release_links": get_footer_release_links()}
