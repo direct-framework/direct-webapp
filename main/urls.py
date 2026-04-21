68c9282f080c9f07a09c96bcb2d4b2405e211a31
Dave Horsfall <dave@horsfall.dev>
2026-04-21 12:11:27 +0100

    add url pattern for policies

--- main/urls.py
+++ main/urls.py
@@ -33,9 +33,15 @@ accounts_patterns = [
     path("overview/", views.AccountOverviewView.as_view(), name="account-overview"),
 ]
 
+policies_patterns = [
+    path("privacy/", views.PrivacyPageView.as_view(), name="privacy"),
+    path("terms/", views.TermsPageView.as_view(), name="terms"),
+    path("governance/", views.GovernancePageView.as_view(), name="governance"),
+    path("licensing/", views.LicensingPageView.as_view(), name="licensing"),
+]
+
 urlpatterns = [
     path("", views.IndexPageView.as_view(), name="index"),
-    path("privacy/", views.PrivacyPageView.as_view(), name="privacy"),
     path(
         "accounts/register/",
         RegistrationView.as_view(form_class=RegistrationForm),
@@ -45,16 +51,14 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include(accounts_patterns)),
     path("about/", views.AboutPageView.as_view(), name="about"),
-    path("terms/", views.TermsPageView.as_view(), name="terms"),
     path(
         "terms-acceptance/",
         views.TermsAcceptanceView.as_view(),
         name="terms_acceptance",
     ),
     path("framework/", include(framework_patterns)),
+    path("policies/", include(policies_patterns)),
     path("get-involved/", views.GetInvolvedPageView.as_view(), name="get_involved"),
     path("events/", views.EventsPageView.as_view(), name="events"),
     path("framework-json/", views.FrameworkView.as_view(), name="framework_json"),
-    path("governance/", views.GovernancePageView.as_view(), name="governance"),
-    path("licensing/", views.LicensingPageView.as_view(), name="licensing"),
 ]
